474ead07e5187cda7ab7a3d7c2614d2fa355b3b0
unternehmen <unternehmen@users.noreply.github.com>
2020-06-20 03:59:08 -0400

    Rendering an image

--- lambhorn.c
+++ lambhorn.c
@@ -22,12 +22,38 @@
 #include <stdarg.h>
 #include "SDL.h"
 
+/* Mark a variable as being used. */
+#define USED(x) ((void)(x))
+
 /* The title of the game window */
 #define WINDOW_TITLE "lambhorn"
 /* The width of the game window in pixels */
 #define WINDOW_WIDTH 640
 /* The height of the game window in pixels */
 #define WINDOW_HEIGHT 480
+
+/* Images are 1-bit bitmaps which can be converted to SDL textures. */
+struct image {
+        int width;
+        int height;
+        char *pixels;
+};
+
+/* The image for the menu cursor */
+static char _cursor_img_pixels[] = {
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 1, 1, 0, 0,
+        1, 1, 1, 1, 1, 0, 1, 0,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 0, 1, 0,
+        0, 0, 0, 0, 1, 1, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0
+};
+static struct image _cursor_img = {8, 8, _cursor_img_pixels};
+
+/* The cursor texture */
+static SDL_Texture *_cursor_tex = NULL;
 
 /* The game window */
 static SDL_Window *_window = NULL;
@@ -47,6 +73,10 @@ void die(const char *format, ...) {
 
 /* Clean up any used memory. */
 static void _clean_up(void) {
+        if (_cursor_tex) {
+                SDL_DestroyTexture(_cursor_tex);
+        }
+
         if (_renderer) {
                 SDL_DestroyRenderer(_renderer);
         }
@@ -56,7 +86,52 @@ static void _clean_up(void) {
         }
 }
 
+/* Create a texture from an image.  print_error may abort. */
+static SDL_Texture *_create_texture_from_image(SDL_Renderer *renderer,
+                                               struct image *image,
+                                               SDL_Color *colors,
+                                               int ncolors,
+                                               void (*print_error)
+                                                 (const char*, ...))
+{
+        SDL_Surface *surf;
+        SDL_Texture *tex;
+
+        surf = SDL_CreateRGBSurfaceWithFormatFrom(
+                   image->pixels,
+                   image->width,
+                   image->height,
+                   8,
+                   image->width * (sizeof *image->pixels),
+                   SDL_PIXELFORMAT_INDEX8);
+        if (!surf) {
+                print_error("SDL_CreateRGBSurface: %s\n", SDL_GetError());
+        }
+
+        if (SDL_SetPaletteColors(surf->format->palette, colors, 0, ncolors) < 0) {
+                const char *error = SDL_GetError();
+                SDL_FreeSurface(surf);
+                print_error("SDL_SetPaletteColors: %s\n", error);
+        }
+
+        tex = SDL_CreateTextureFromSurface(renderer, surf);
+        if (!tex) {
+                const char *error = SDL_GetError();
+                SDL_FreeSurface(surf);
+                print_error("SDL_CreateTextureFromSurface: %s\n", error);
+        }
+
+        SDL_FreeSurface(surf);
+
+        return tex;
+}
+
 int main(int argc, char *argv[]) {
+        SDL_Color colors[2];
+
+        USED(argc);
+        USED(argv);
+
         /* Initialize SDL. */
         if (SDL_Init(SDL_INIT_VIDEO) == -1) {
                 die("SDL_Init: %s\n", SDL_GetError());
@@ -83,6 +158,23 @@ int main(int argc, char *argv[]) {
                 die("SDL_CreateRenderer: %s\n", SDL_GetError());
         }
 
+        /* Set up the master palette. */
+        colors[0].r = 0xff;
+        colors[0].g = 0xff;
+        colors[0].b = 0xff;
+        colors[0].a = 0xff;
+        colors[1].r = 0x00;
+        colors[1].g = 0x00;
+        colors[1].b = 0x00;
+        colors[1].a = 0xff;
+
+        /* Create the cursor texture. */
+        _cursor_tex = _create_texture_from_image(_renderer,
+                                                 &_cursor_img,
+                                                 colors,
+                                                 2,
+                                                 &die);
+
         /* Loop until the game ends. */
         while (1) {
                 SDL_Event event;
@@ -107,6 +199,7 @@ int main(int argc, char *argv[]) {
                                        0xff, 0xff, 0xff,
                                        SDL_ALPHA_OPAQUE);
                 SDL_RenderClear(_renderer);
+                SDL_RenderCopy(_renderer, _cursor_tex, NULL, NULL);
                 SDL_RenderPresent(_renderer);
 
                 /* Delay until the next frame. */
