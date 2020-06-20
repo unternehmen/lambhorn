1cbb2c484c0fe39a06a17dabcdbe58b2309fc769
Chris Murphy <unternehmen@users.noreply.github.com>
2020-06-20 03:43:05 +0000

    Initial commit

---
+++ lambhorn.c
@@ -0,0 +1,118 @@
+/* lambhorn.c - a fantasy RPG
+ * Copyright (c) 2020 Tofu Taco Co-op
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * .
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * .
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "SDL.h"
+
+/* The title of the game window */
+#define WINDOW_TITLE "thorn"
+/* The width of the game window in pixels */
+#define WINDOW_WIDTH 640
+/* The height of the game window in pixels */
+#define WINDOW_HEIGHT 480
+
+/* The game window */
+static SDL_Window *_window = NULL;
+/* The renderer for drawing all graphics */
+static SDL_Renderer *_renderer = NULL;
+
+/* Abort the program with an error message. */
+void die(const char *format, ...) {
+        va_list ap;
+
+        va_start(ap, format);
+        vfprintf(stderr, format, ap);
+        va_end(ap);
+
+        exit(EXIT_FAILURE);
+}
+
+/* Clean up any used memory. */
+static void _clean_up(void) {
+        if (_renderer) {
+                SDL_DestroyRenderer(_renderer);
+        }
+
+        if (_window) {
+                SDL_DestroyWindow(_window);
+        }
+}
+
+int main(int argc, char *argv[]) {
+        /* Initialize SDL. */
+        if (SDL_Init(SDL_INIT_VIDEO) == -1) {
+                die("SDL_Init: %s\n", SDL_GetError());
+        }
+        atexit(&SDL_Quit);
+
+        /* Set up an exit handler to free any used memory. */
+        atexit(&_clean_up);
+
+        /* Open the window. */
+        _window = SDL_CreateWindow(WINDOW_TITLE,
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   WINDOW_WIDTH,
+                                   WINDOW_HEIGHT,
+                                   0);
+        if (!_window) {
+                die("SDL_CreateWindow: %s\n", SDL_GetError());
+        }
+
+        /* Create the renderer. */
+        _renderer = SDL_CreateRenderer(_window, -1, 0);
+        if (!_renderer) {
+                die("SDL_CreateRenderer: %s\n", SDL_GetError());
+        }
+
+        /* Loop until the game ends. */
+        while (1) {
+                SDL_Event event;
+
+                /* Handle user input. */
+                while (SDL_PollEvent(&event)) {
+                        if (event.type == SDL_QUIT) {
+                                exit(EXIT_SUCCESS);
+                        } else if (event.type == SDL_KEYDOWN) {
+                                switch (event.key.keysym.scancode) {
+                                        case SDL_SCANCODE_ESCAPE:
+                                                exit(EXIT_SUCCESS);
+                                                break;
+                                        default:
+                                                break;
+                                }
+                        }
+                }
+
+                /* Draw the scene. */
+                SDL_SetRenderDrawColor(_renderer,
+                                       0xff, 0xff, 0xff,
+                                       SDL_ALPHA_OPAQUE);
+                SDL_RenderClear(_renderer);
+                SDL_RenderPresent(_renderer);
+
+                /* Delay until the next frame. */
+                SDL_Delay(40);
+        }
+
+        /* The program never reaches here. */
+        return 0;
+}
